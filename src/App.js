a6d0a536d2485be5281200953d253e13047ba0d8
new name1 <nitilsi@mail.com>
2023-02-06 22:38:29 +0530

    update build for gh-pages101

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import React, { useState } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import LoadingBar from 'react-top-loading-bar'
 
@@ -15,7 +15,7 @@ const App = ()=> {
     return (
       <div>
         <h1>hello shreya</h1>
-        <HashRouter>
+        <BrowserRouter>
         <NavBar/> 
         {/* <LoadingBar
         height={3}
@@ -32,7 +32,7 @@ const App = ()=> {
           <Route exact path="/sports" element={<News setProgress={setProgress} apiKey={apiKey} key="sports" pageSize={pageSize} country="in" category="sports"/> }/>
           <Route exact path="/technology" element={<News setProgress={setProgress} apiKey={apiKey} key="technology" pageSize={pageSize} country="in" category="technology"/>}/> 
         </Routes>
-        </HashRouter>
+        </BrowserRouter>
       </div>
     )
  
